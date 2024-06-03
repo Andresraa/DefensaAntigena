ab91b253e6ed26d98329465e7256592759e2a77b
Andresraa <149993948+Andresraa@users.noreply.github.com>
2024-06-03 11:53:11 -0500

    Create Datos_aliado.py
    Datos de aliado

---
+++ codigo/Datos_aliado.py
@@ -0,0 +1,22 @@
+DATOS_DE_TORRETA = [
+  {
+    #1
+    "rango": 90,
+    "enfriamiento": 1500,
+  },
+  {
+    #2
+    "rango": 110,
+    "enfriamiento": 1200,
+  },
+  {
+    #3
+    "rango": 125,
+    "enfriamiento": 1000,
+  },
+  {
+    #4
+    "rango": 150,
+    "enfriamiento": 900,
+  }
+]
